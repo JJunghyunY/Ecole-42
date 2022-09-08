f76686c2bcc18e152d162f8b23f076012bc2c1b1
JJunghyunY <wjdgus995@hanmail.net>
2022-09-08 09:37:51 +0900

    jebal

--- listgnl/get_next_line_utils.c
+++ listgnl/get_next_line_utils.c
@@ -32,84 +32,48 @@ char	*my_strdup(const char *s1)
 
 char	*my_substr(char const *s, unsigned int start, size_t len)
 {
-	// char	*src;
-	// size_t	i;
-	// size_t	slen;
-
-	// if (!s)
-	// 	return (NULL);
-	// slen = my_strlen(s);
-	// if (start > slen)
-	// 	return (my_strdup(""));
-	// if (slen > len)
-	// 	src = (char *)malloc(sizeof(char) * len + 1);
-	// if (slen <= len)
-	// 	src = (char *)malloc(sizeof(char) * slen - start + 1);
-	// if (!src)
-	// 	return (NULL);
-	// i = 0;
-	// while (i < len && s[start])
-	// {
-	// 	src[i] = s[start];
-	// 	start++;
-	// 	i++;
-	// }
-	// src[i] = '\0';
-	// return (src);
-    char	*p;
+    char	*src;
 	size_t	i;
-	size_t	num_chars;
+	size_t	slen;
 
-	i = 0;
-	num_chars = 0;
 	if (!s || len <= 0)
 		return (NULL);
+	i = 0;
+	slen = 0;
 	if (start < my_strlen(s))
-		num_chars = my_strlen(s) - (size_t)start;
-	if (len < num_chars)
-		num_chars = len;
-	p = (char *)malloc((num_chars + 1) * sizeof(char));
-	if (!p)
+		slen = my_strlen(s) - (size_t)start;
+	if (len < slen)
+		slen = len;
+	src = (char *)malloc((slen + 1) * sizeof(char));
+	if (!src)
 		return (NULL);
-	while (num_chars && start < my_strlen(s))
+	while (slen && start < my_strlen(s))
 	{
-		p[i] = ((char *)s)[start + i];
+		src[i] = ((char *)s)[start + i];
 		i++;
-		num_chars--;
+		slen--;
 	}
-	p[i] = '\0';
-	return (p);
+	src[i] = '\0';
+	return (src);
 }
 
 char	*my_strchr(const char *s, int c)
 {
-	// int				i;
-	// unsigned char	ch;
+	int				i;
+	unsigned char	ch;
 
-	// if(!s)
-	// 	return (NULL);
-	// ch = (unsigned char)c;
-	// i = 0;
-	// while (s[i])
-	// {
-	// 	if (s[i] == ch)
-	// 		return ((char *)s + i +1);
-	// 	i++;
-	// }
-	// if (s[i] == ch)
-	// 	return ((char *)s + i);
-	// return (NULL);
-    int	i;
-
+	if(!s)
+		return (NULL);
+	ch = (unsigned char)c;
 	i = 0;
-	if (!s)
-		return (0);
 	while (s[i])
-	{	
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
+	{
+		if (s[i] == ch)
+			return ((char *)s + i +1);
 		i++;
 	}
+	if (s[i] == ch)
+		return ((char *)s + i);
 	return (NULL);
 }
 
